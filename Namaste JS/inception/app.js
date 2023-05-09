50852dce0eadb047e1f5e37e9b9347c5a71b4383
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-05-09 19:28:09 +0530

    added Inception leacture notes

--- Namaste JS/inception/app.js
+++ Namaste JS/inception/app.js
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom';
 
 
 
-
-
-
 const h11 = React.createElement("h1", {}, "Hello world")
 const h12 = React.createElement("h1", {}, "Rutvik")
 
 const outerDiv = React.createElement("div", {
-    id: "Hellow",
-    className: "h1Div"
+    id: "Hellow"
 },
     [h11, h12])
 
 let reactAPP = ReactDOM.createRoot(document.getElementById("main"))
 reactAPP.render(outerDiv)
 
+console.log("Checking wheather log is remove or not!");
 // let reactAPP = ReactDOM.createRoot(document.getElementById("main"))
 // reactAPP.render(h1Tag)
